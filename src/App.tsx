87ee6e29a3626ba9688da1b83e1c0fded71b2e7d
Evan Qi <evanqi@gmail.com>
2022-04-20 23:03:51 -0400

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -14,27 +14,49 @@ function App() {
   const [ carLocationList, setCarLocationList ] = useState(new Set<locationObject>());
   const [ inputScreen, setInputScreen ] = useState(Screens.DeliveryLocations);
   const [ res, setRes ] = useState([""]);
+  const [ isLoading, setIsLoading ] = useState(false);
 
   return (
     <div className="App">
       <p>
-        {inputScreen === Screens.DeliveryLocations ? 'Deliveries' : 'Cars'}
-      </p> 
-      <select onChange={(e) => setInputScreen(e.target.value === '0' ? 0 : 1)}>
-        <option value='0'>Delivery</option>
-        <option value='1'>Car</option>
-      </select>
-      {inputScreen === Screens.DeliveryLocations 
-      ? <LocationList setList={setLocationList} list={locationList} inputType={inputScreen}></LocationList> 
-      : <LocationList setList={setCarLocationList} list={carLocationList} inputType={inputScreen}></LocationList>}
-      <Input setLocationList={setLocationList} setCarLocationList={setCarLocationList} inputType={inputScreen}></Input>
-      <Calculate locationList={locationList} carList={carLocationList} setRes={setRes}></Calculate>
+        {inputScreen === Screens.DeliveryLocations ? <p>Places to visit!<br></br>Please enter 1 or more delivery locations</p> : <p>Initial locations of drivers!<br></br>Please enter 1 or more initial car location.</p>}
+      </p>
+      
+      <div className="border">
+        <div className="inline">
+          <select onChange={(e) => setInputScreen(e.target.value === '0' ? 0 : 1)}>
+            <option value='0'>Delivery</option>
+            <option value='1'>Cars</option>
+          </select>
+        </div> 
+        <div className="inline">
+          <Calculate locationList={locationList} carList={carLocationList} setRes={setRes} setIsLoading={setIsLoading}></Calculate>
+        </div>
+      </div>
       <div>
-        {res.map((url: string) => {
-          return <div>
-            <a href={`${url}`}>{url}</a>
-            </div>
-        })}
+        <Input setLocationList={setLocationList} setCarLocationList={setCarLocationList} inputType={inputScreen}></Input>
+      </div>
+      <div className="border">
+        {inputScreen === Screens.DeliveryLocations 
+        ? <LocationList setList={setLocationList} list={locationList} inputType={inputScreen}></LocationList> 
+        : <LocationList setList={setCarLocationList} list={carLocationList} inputType={inputScreen}></LocationList>}
+      </div>
+      <div>
+        {isLoading 
+        ? <div>Calculating...</div>
+        : res.map((url: string) => {
+          if (url.startsWith("http")) {
+            return <div>
+                    <a href={`${url}`}>{url}</a>
+                    </div>
+          } else {
+            return <div>{url}</div>
+          } 
+          }
+        )}
+      </div>
+      <div>
+
       </div>
     </div>
   );
--- src/components/Calculate.tsx
+++ src/components/Calculate.tsx
@@ -6,9 +6,10 @@ export interface CalculateProps {
   locationList: Set<locationObject>
   carList: Set<locationObject>
   setRes: Function
+  setIsLoading: Function
 }
 
-const calcRes = (locationList: Set<locationObject>, carList: Set<locationObject>, setRes: Function): void => {
+const calcRes = (locationList: Set<locationObject>, carList: Set<locationObject>, setRes: Function, setIsLoading: Function): void => {
   const headers = {
     'Content-Type': 'application/json'
   }
@@ -21,16 +22,22 @@ const calcRes = (locationList: Set<locationObject>, carList: Set<locationObject>
   })
   .then((response) => {
     console.log(response)
+    setIsLoading(false)
     setRes(response.data)
   })
   .catch((error) => {
     console.log(error)
+    setIsLoading(false)
+    setRes(["Bad Input"])
   })
 }
 
-const Calculate: React.FC<CalculateProps> = ({locationList, carList, setRes}) => {
+const Calculate: React.FC<CalculateProps> = ({locationList, carList, setRes, setIsLoading}) => {
   return (
-    <button className="inline" onClick={() => calcRes(locationList, carList, setRes)}>calculate!</button>
+    <button className="inline" onClick={() => {
+      setIsLoading(true);
+      calcRes(locationList, carList, setRes, setIsLoading);
+    }}>calculate!</button>
   )
 }
 
